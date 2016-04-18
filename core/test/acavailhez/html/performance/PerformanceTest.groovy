7dcdd83b5d85b7771836ce54c6263d7ab38c8e99
Arnaud CAVAILHEZ <arnaud@getlocalmotion.com>
2016-04-17 20:57:23 -0700

    Performance comparison against velocity

--- core/test/acavailhez/html/performance/PerformanceTest.groovy
+++ core/test/acavailhez/html/performance/PerformanceTest.groovy
@@ -2,17 +2,27 @@ package acavailhez.html.performance
 
 import acavailhez.html.AbstractTest
 import acavailhez.html.Html
+import acavailhez.html.HtmlStyle
+import org.apache.velocity.Template
+import org.apache.velocity.VelocityContext
+import org.apache.velocity.app.VelocityEngine
 import org.junit.Test
 
 public class PerformanceTest extends AbstractTest {
 
     class PerfHtml extends Html {
 
+        private final String hello
+
+        public PerfHtml(String hello) {
+            this.hello = hello
+        }
+
         @Override
         protected void build() {
             div(class: 'box') {
                 div(class: 'box-body') {
-                    escape << "content of the box"
+                    escape << "Hello " + hello
                 }
             }
         }
@@ -20,14 +30,40 @@ public class PerformanceTest extends AbstractTest {
 
     @Test
     public void test1000() throws Exception {
-        long ms = System.currentTimeMillis()
+
+        long ms, diff
+
+        // html-groovy-renderer
+        ms = System.currentTimeMillis()
+        Html html = new PerfHtml("World")
         for (int i = 0; i < 1000; i++) {
-            new PerfHtml().getRawHtml()
+            html.getRawHtml()
         }
-        long diff = System.currentTimeMillis() - ms
+        diff = System.currentTimeMillis() - ms
         println 'Simple HTML 1000:' + diff + 'ms'
-        // allow less than a ms to render this
-        assert diff < 10000
+
+        ms = System.currentTimeMillis()
+        for (int i = 0; i < 1000; i++) {
+            new PerfHtml("World").withStyle(HtmlStyle.PRETTY).getRawHtml()
+        }
+        diff = System.currentTimeMillis() - ms
+        println 'Simple style HTML 1000:' + diff + 'ms'
+
+        // velocity
+        VelocityEngine ve = new VelocityEngine();
+        ve.init();
+        ms = System.currentTimeMillis()
+        for (int i = 0; i < 1000; i++) {
+            Template t = ve.getTemplate("./test-resources/simple.vm");
+            VelocityContext context = new VelocityContext();
+            context.put("hello", "World")
+            StringWriter writer = new StringWriter();
+            t.merge(context, writer);
+            writer.toString()
+        }
+        diff = System.currentTimeMillis() - ms
+        println 'Velocity Simple HTML 1000:' + diff + 'ms'
+
     }
 
     @Test
@@ -38,7 +74,6 @@ public class PerformanceTest extends AbstractTest {
         }
         long diff = System.currentTimeMillis() - ms
         println 'Simple HTML 1000:' + diff + 'ms'
-        // allow less than a ms to render this
-        assert diff < 10000
+
     }
 }
