881017c313b6d981021256c079d787cc4d336e12
Arnaud CAVAILHEZ <arnaud@getlocalmotion.com>
2016-04-17 21:06:04 -0700

    Added performances tests on bootstrap modal

--- core/test/acavailhez/html/performance/PerformanceTest.groovy
+++ core/test/acavailhez/html/performance/PerformanceTest.groovy
@@ -3,6 +3,8 @@ package acavailhez.html.performance
 import acavailhez.html.AbstractTest
 import acavailhez.html.Html
 import acavailhez.html.HtmlStyle
+import acavailhez.html.traits.bootstrap.Bootstrap4ButtonStyle
+import acavailhez.html.traits.bootstrap.Bootstrap4Trait
 import org.apache.velocity.Template
 import org.apache.velocity.VelocityContext
 import org.apache.velocity.app.VelocityEngine
@@ -35,19 +37,19 @@ public class PerformanceTest extends AbstractTest {
 
         // html-groovy-renderer
         ms = System.currentTimeMillis()
-        Html html = new PerfHtml("World")
-        for (int i = 0; i < 1000; i++) {
-            html.getRawHtml()
-        }
-        diff = System.currentTimeMillis() - ms
-        println 'Simple HTML 1000:' + diff + 'ms'
-
-        ms = System.currentTimeMillis()
         for (int i = 0; i < 1000; i++) {
             new PerfHtml("World").withStyle(HtmlStyle.PRETTY).getRawHtml()
         }
         diff = System.currentTimeMillis() - ms
         println 'Simple style HTML 1000:' + diff + 'ms'
+
+        ms = System.currentTimeMillis()
+        for (int i = 0; i < 1000; i++) {
+            new PerfHtml("World").getRawHtml()
+        }
+        diff = System.currentTimeMillis() - ms
+        println 'Simple HTML 1000:' + diff + 'ms'
+
 
         // velocity
         VelocityEngine ve = new VelocityEngine();
@@ -57,6 +59,66 @@ public class PerformanceTest extends AbstractTest {
             Template t = ve.getTemplate("./test-resources/simple.vm");
             VelocityContext context = new VelocityContext();
             context.put("hello", "World")
+            StringWriter writer = new StringWriter();
+            t.merge(context, writer);
+            writer.toString()
+        }
+        diff = System.currentTimeMillis() - ms
+        println 'Velocity Simple HTML 1000:' + diff + 'ms'
+
+    }
+
+    class ModalHtml extends Html implements Bootstrap4Trait {
+
+        private final String title
+        private final String content
+
+        public ModalHtml(String title, String content) {
+            this.title = title
+            this.content = content
+        }
+
+        @Override
+        protected void build() {
+            bootstrapModal(title: title, closeLabel: "Close") {
+                p(content)
+            } {
+                bootstrapButton("Close", 'data-dismiss': 'modal')
+                bootstrapButton("Save changes", btnStyle: Bootstrap4ButtonStyle.PRIMARY)
+            }
+        }
+    }
+
+    @Test
+    public void test1000Modals() throws Exception {
+
+        long ms, diff
+
+        // html-groovy-renderer
+        ms = System.currentTimeMillis()
+        for (int i = 0; i < 1000; i++) {
+            new ModalHtml("World", "One fine body…").withStyle(HtmlStyle.PRETTY).getRawHtml()
+        }
+        diff = System.currentTimeMillis() - ms
+        println 'Simple style HTML 1000:' + diff + 'ms'
+
+        ms = System.currentTimeMillis()
+        for (int i = 0; i < 1000; i++) {
+            new ModalHtml("World", "One fine body…").getRawHtml()
+        }
+        diff = System.currentTimeMillis() - ms
+        println 'Simple HTML 1000:' + diff + 'ms'
+
+
+        // velocity
+        VelocityEngine ve = new VelocityEngine();
+        ve.init();
+        ms = System.currentTimeMillis()
+        for (int i = 0; i < 1000; i++) {
+            Template t = ve.getTemplate("./test-resources/modal.vm");
+            VelocityContext context = new VelocityContext();
+            context.put("title", "World")
+            context.put("content", "One fine body…")
             StringWriter writer = new StringWriter();
             t.merge(context, writer);
             writer.toString()
